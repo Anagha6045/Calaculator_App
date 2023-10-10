c8b1702f772c4bf53b044ac6f9a0a8e0df52c4cd
anagh <anagh@LAPTOP-E2KT3BQR>
2023-10-10 06:55:18 +0530

    A calculator application

---
+++ calculator_app/src/main/java/calculator_app/Calculator.java
@@ -0,0 +1,21 @@
+package calculator_app;
+
+import java.util.Scanner;
+
+
+public class Calculator {
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" Calculator Application /n");
+		System.out.println("Enter the numbers : ");
+		System.out.print("No1 : "  );
+		double n1=sc.nextDouble();
+		System.out.print("No2 : " );
+		double n2=sc.nextDouble();
+		
+	}
+	
+	
+}
